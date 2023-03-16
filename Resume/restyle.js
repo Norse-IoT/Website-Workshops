1e225ce749e818d025c198b420fa71a44a8f0d08
CG <congodsted@protonmail.com>
2023-03-16 16:47:00 -0400

    Add download button

--- Resume/restyle.js
+++ Resume/restyle.js
@@ -3,8 +3,9 @@ let style = [
     {name:"Red Resume", link:"red.css"},
     {name:"Green Resume", link:"green.css"}
     ]
+
 window.addEventListener("load", function(){
-    document.querySelector("nav").innerHTML = `<div style="font-family: sans-serif;padding-bottom: 5px;">Norse IoT Resume Workshops</div><div id="projects"></div>`
+    document.querySelector("nav").innerHTML = `<div style="font-family: sans-serif;padding-bottom: 5px;">Norse IoT Resume Workshops</div><div style="margin-bottom:.5em" id="projects"></div>`
     style.forEach(function(css) {
         if (css.link.search(".css") != -1) {
             document.querySelector("#projects").innerHTML += `<button onclick="changeStyle('${css.link}')">${css.name}</button>`
@@ -15,8 +16,10 @@ window.addEventListener("load", function(){
 
         }
     })
+    document.querySelector("nav").innerHTML += `<a download href="https://norse-iot.github.io/Website-Workshops/Resume/index.html"><button>Download Template</button></a><a class='css-download' download href="https://norse-iot.github.io/Website-Workshops/Resume/red.css"><button>Download CSS</button></a>`
 });
 
 function changeStyle(style) {
     document.querySelector("link").href = style
+    document.querySelector(".css-download").href = `https://norse-iot.github.io/Website-Workshops/Resume/${style}`
 }
